d512e2a29b7aac920285a3265002706b4cf6a861
n20va <99407852+n20va@users.noreply.github.com>
2025-06-29 20:15:37 +0300

    Update HistoryManager.java

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -4,11 +4,7 @@ import model.Task;
 import java.util.List;
 
 public interface HistoryManager {
-    
     void add(Task task);
-    
     void remove(int id);
-    
-    List<Task> getHistory();
-    
+    List<Task> getHistory();  
 }
