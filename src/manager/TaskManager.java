3b9fbe30c154d52a706a8a797c9d929d4906894e
n20va <99407852+n20va@users.noreply.github.com>
2025-06-29 20:20:38 +0300

    Update TaskManager.java

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -9,27 +9,39 @@ import java.util.List;
 public interface TaskManager {
 
     void addTask(Task task);
+
     void addEpic(Epic epic);
+
     void addSubtask(Subtask subtask);
 
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void removeTaskById(int id);
+
     void removeEpicById(int id);
+
     void removeSubtaskById(int id);
 
     void clearTasks();
+
     void clearEpics();
+
     void clearSubtasks();
 
     List<Subtask> getSubtasksOfEpic(int epicId);
